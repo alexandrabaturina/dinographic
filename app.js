fb80efabd23ba8c61b58bed01e617f5f1e5f6c2c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-09-01 10:39:47 -0700

    chore: create Dino constructor

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+function Dino(species, weight, height, diet, habitat, period, fact, image) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.habitat = habitat;
+    this.period = period;
+    this.fact = fact;
+    this.image = image;
+}
 
-
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Objects
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Human Object
 
-    // Remove form from screen
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+    // Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
