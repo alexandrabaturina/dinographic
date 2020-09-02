913dff692fc3bf2f953e9d34701289575b152352
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-09-02 12:02:33 -0700

    feat: compare diets

--- app.js
+++ app.js
@@ -22,25 +22,7 @@ function Human(name, weight, height, diet) {
     this.image = "human.png";
 }
 
-// Read JSON data
-
-// async function getData() {
-
-//     try {
-//         let response = await fetch('dino.json');
-//         let promise = await response.json();
-//         let data = await response.data;
-//         return data
-//     } catch (err) {
-//         alert(err);
-//     }
-// }
-
-// const jsonData = getData();
-
-
 // Create Dino Objects
-
 const triceratops = new Dino("Triceratops", 13000, 114, "herbavor", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh.", "triceratops.png");
 const tyrannosaurusRex = new Dino("Tyrannosaurus Rex", 11905, 144, "carnivor", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.", "tyrannosaurus rex.png");
 const anklyosaurus = new Dino("Anklyosaurus", 10500, 55, "herbavor", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.", "anklyosaurus.png");
@@ -50,64 +32,83 @@ const elasmosaurus = new Dino("Elasmosaurus", 16000, 59, "carnivor", "North Amer
 const ptaranodon = new Dino("Pteranodon", 44, 20, "carnivor", "North America", "Late Cretaceous", "Actually a flying reptile, the Pteranodon is not a dinosaur.", "pteranodon.png");
 const pigeon = new Dino("Pigeon", 0.5, 9, "herbavor", "World Wide", "Holocene", "All birds are living dinosaurs", "pigeon.png");
 
+const dinosaurs = [triceratops, tyrannosaurusRex, anklyosaurus, brachiosaurus, stegosaurus, elasmosaurus, ptaranodon, pigeon];
+
 // Get human data from form
 function getHumanDataFromForm() {
-    const name = document.getElementById('name').value;
-    const weight = document.getElementById('weight').value;
-    const height = Number(document.getElementById('inches').value) +
-        Number(document.getElementById('feet').value) * 12;
-    const diet = document.getElementById('diet').value;
-    return name, weight, height, diet;
+    const name = document.getElementById("name").value;
+    const weight = document.getElementById("weight").value;
+    const height = Number(document.getElementById("inches").value) +
+        Number(document.getElementById("feet").value) * 12;
+    const diet = document.getElementById("diet").value;
+    human = new Human(name, weight, height, diet);
+    return human;
 };
 
 
 function showInfographics() {
-    human = new Human(getHumanDataFromForm());
-    generateInfographics(human);
+    getHumanDataFromForm();
+    generateInfographics();
     dinoCompare.style.display = 'none';
-    infographicsGrid.style.visibility = 'visible';
+    infographicsGrid.style.display = 'flex';
 }
 
 function generateInfographics() {
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
-    createTile(triceratops);
+    createTile(dinosaurs[0]);
+    createTile(dinosaurs[1]);
+    createTile(dinosaurs[2]);
+    createTile(dinosaurs[3]);
+    createTile(dinosaurs[4]);
+    createTile(dinosaurs[5]);
+    createTile(dinosaurs[6]);
+    createTile(dinosaurs[7]);
 }
 
 function createTile(dinosaur) {
+    if (dinosaur.species !== 'Pigeon') {
+        const facts = [dinosaur.fact, compareWeight(dinosaur), compareHeight(dinosaur), compareDiet(dinosaur)];
+        fact = getRandomFact(facts);
+    } else {
+        fact = dinosaur.fact;
+    }
     const tile = document.createElement('div');
     tile.classList.add('grid-item');
     tile.innerHTML = `
     <h3>${dinosaur.species}</h3>
     <img src="images/${dinosaur.image}">
-    <p> Fact goes here! </p>
-
+    <p>${fact}</p>
     `;
     infographicsGrid.appendChild(tile);
 }
 
-
 // Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches. 
+function compareWeight(dinosaur) {
 
+};
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
-
+function compareHeight(dinosaur) {
+    return 'Height fact';
+};
 
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
+function compareDiet(dinosaur) {
+    if (dinosaur.diet === human.diet.toLowerCase()) {
+        return `Both you and ${dinosaur.species} have ${dinosaur.diet} diet.`
+    } else {
+        return `Unlike you, ${dinosaur.species} has ${dinosaur.diet} diet.`
+    }
+};
 
 
-// Generate Tiles for each Dino in Array
-
+// Get random fact
+getRandomFact = function (list) {
+    return list[Math.floor((Math.random() * list.length))];
+}
 
 
 // On button click, display infographic
+infographicsGrid.style.display = 'none';
 document.getElementById('btn').addEventListener("click", showInfographics);
